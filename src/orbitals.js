9a384b1881dfe8c7328077a8c9a1be7dadca5bbc
Felipe Domingues <felipeeu@yahoo.com.br>
2018-10-05 18:46:29 -0300

    implement some functions and tests
    -orbital values
    -principalQuantumNumbers
    -angularQuantumNumbers
    -subShellEnergy
    -sortSubShell
    -subShellSequence

--- src/orbitals.js
+++ src/orbitals.js
@@ -3,6 +3,7 @@ empty orbital ==>  [0,0] ;
 half full orbital ==> [1,0] ;
 full orbital ==> [1,1]
 * */
+const {flatten} = require('lodash')
 
 const numberOfOrbitals = (n, l = []) => {
 
@@ -39,11 +40,45 @@ const orbitalTypes = (l) => {
 
         case 6:
             return "i";
+        case 7 :
+            return "j"
 
         default:
             return null
     }
 };
+
+const orbitalValues = (subshell) => {
+    switch (subshell) {
+
+        case "s":
+            return 0;
+
+        case "p":
+            return 1;
+
+        case "d":
+            return 2;
+
+        case "f":
+            return 3;
+
+        case "g":
+            return 4;
+
+        case "h":
+            return 5;
+
+        case "i":
+            return 6;
+        case "j" :
+            return 7
+
+        default:
+            return null
+    }
+};
+
 
 const totalSquares = (l) => {
     return 2 * l + 1
@@ -59,24 +94,89 @@ const possibleElectrons = (l, Q = []) => {
     return Q
 };
 
-const magneticNumbers = (l, ml = new Map(), o = [0,0]) => {
+const magneticNumbers = (l, ml = new Map(), o = [0, 0]) => {
 
     let i;
 
     for (i = -l; i <= l; i++) {
 
-        ml.set(i , o)
+        ml.set(i, o)
 
     }
     return ml
 };
 
+const principalQuantumNumber = (subshell) => {
+    return parseInt(subshell.substr(0, 1), 10)
+};
+
+const angularQuantumNumber = (subshell) => {
+    return orbitalValues(subshell.substr(1))
+}
+
+const subShellEnergy = (subshell) => {
+
+    let n = principalQuantumNumber(subshell)
+    let l = angularQuantumNumber(subshell)
+
+    return n + l
+};
+
+const sortSubShell = (a, b) => {
+
+
+    let a1 = subShellEnergy(a)
+    let b1 = subShellEnergy(b)
+
+    let a2 = principalQuantumNumber(a)
+    let b2 = principalQuantumNumber(b)
+
+    if (a1 > b1) {
+        return -1;
+
+    } else if (b1 > a1) {
+        return 1
+
+    } else if (b2 > a2) {
+        return 1
+    } else if (a2 > b2) {
+        return -1
+    } else {
+        return 0
+    }
+};
+
+const subShellSequence = () => {
+    n = [1, 2, 3, 4, 5, 6, 7, 8];
+
+
+    let seq = n.map((npar) => numberOfOrbitals(npar).map((lpar) => {
+
+
+        let x = npar + orbitalTypes(lpar)
+
+        return x
+    }));
+    let z = flatten(seq)
+    let newSeq = z.sort(sortSubShell)
+    return newSeq.reverse()
+
+};
+
+
 module.exports = {
     numberOfOrbitals,
     orbitalTypes,
+    orbitalValues,
     totalSquares,
     possibleElectrons,
-    magneticNumbers
+    magneticNumbers,
+    principalQuantumNumber,
+    angularQuantumNumber,
+    subShellEnergy,
+    sortSubShell,
+    subShellSequence,
+
 };
 
 
--- src/orbitals.test.js
+++ src/orbitals.test.js
@@ -1,4 +1,19 @@
-const {numberOfOrbitals, orbitalTypes, totalSquares, possibleElectrons, magneticNumbers} = require('./orbitals');
+const
+    {
+        numberOfOrbitals,
+        orbitalTypes,
+        orbitalValues,
+        totalSquares,
+        possibleElectrons,
+        magneticNumbers,
+        principalQuantumNumber,
+        angularQuantumNumber,
+        subShellEnergy,
+        sortSubShell,
+        subShellSequence,
+
+    }
+        = require('./orbitals');
 
 
 describe('numbers of orbitals', () => {
@@ -34,6 +49,21 @@ describe('orbital types', () => {
     })
 });
 
+describe('orbital Values', () => {
+
+    it('s orbital ', () => {
+        expect(orbitalValues("s")).toEqual(0)
+    });
+    it('p orbital', () => {
+        expect(orbitalValues("p")).toEqual(1)
+    });
+    it('d orbital', () => {
+        expect(orbitalValues("d")).toEqual(2)
+    });
+    it('sem orbital', () => {
+        expect(orbitalValues("i")).toEqual(6)
+    })
+});
 
 describe('Total Squares', () => {
 
@@ -91,5 +121,63 @@ describe('magnetic quantum numbers', () => {
 
     it('case l = 3 ', () => {
         expect(magneticNumbers(3)).toEqual(lThree)
+    });
+});
+
+
+describe('Principal quantum number', () => {
+
+    it('case 3d ', () => {
+        expect(principalQuantumNumber("3d")).toEqual(3)
+    });
+    it('case 5d ', () => {
+        expect(principalQuantumNumber("5d")).not.toEqual(8)
+    });
+});
+
+describe('Angular quantum number', () => {
+
+    it('case 3d ', () => {
+        expect(angularQuantumNumber("3d")).toEqual(2)
+    });
+    it('case 5d ', () => {
+        expect(angularQuantumNumber("5d")).not.toEqual(8)
+    });
+});
+
+
+
+
+describe('subshell sequence', () => {
+
+    test.only('Total sequence  ', () => {
+        expect(subShellSequence(3)).toEqual(4)
+    });
+
+});
+
+
+describe('sort subshells ', () => {
+
+    it('case  4p / 5s  ', () => {
+        expect(sortSubShell("4p", "5s")).toEqual(1)
+    });
+
+    it('case  4s / 3d  ', () => {
+        expect(sortSubShell("4s", "3d")).toEqual(1)
+    });
+});
+
+
+describe('subshell energy', () => {
+
+    it('case 4s ', () => {
+        expect(subShellEnergy("4s")).toEqual(4)
+    });
+    it('case 6d ', () => {
+        expect(subShellEnergy("6d")).toEqual(8)
+    });
+    it('case 3p ', () => {
+        expect(subShellEnergy("3p")).not.toEqual(8)
     });
 });
