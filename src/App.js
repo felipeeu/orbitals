adbd898e71df1478143537a4881d61ac77349598
Felipe Domingues <felipeeu@yahoo.com.br>
2018-10-15 07:27:28 -0300

    remove react logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import AllOrbitals from './components/AllOrbitals.js'
 
@@ -10,7 +9,6 @@ class App extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
                     <h1 className="App-title">Distribuição Eletrônica</h1>
                 </header>
                 <AllOrbitals/>
